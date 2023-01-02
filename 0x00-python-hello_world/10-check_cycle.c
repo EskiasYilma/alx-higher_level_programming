fb075bb0705f0d1e4739737ad7cbf67cc4c4b3a8
exuszee <exuszee@gmail.com>
2023-01-02 17:30:31 +0300

    fix 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,9 +11,13 @@ int check_cycle(listint_t *list)
 	listint_t *a = list;
 	listint_t *b = list->next;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 	{
 		return (0);
+	}
+	if (list->next == list)
+	{
+		return (1);
 	}
 
 	while (a != NULL && b != NULL && b->next != NULL)
