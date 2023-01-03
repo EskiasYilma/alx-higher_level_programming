37beb9be56e6288ae872db600a88fd31e65f5e19
exuszee <exuszee@gmail.com>
2023-01-03 16:52:18 +0300

    fix 7 - 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for i, j in enumerate(str):
-		if ord(j) >= 97 and ord(j) <= 122:
-			print('{:c}'.format(ord(j)-32), end="\n" if i == len(str)-1 else '')
-		else:
-			print('{:c}'.format(ord(j)), end="\n" if i == len(str)-1 else '')
+    for i, j in enumerate(str):
+        if ord(j) >= 97 and ord(j) <= 122:
+            print('{:c}'.format(ord(j) - 32),
+                  end="\n" if i == len(str) - 1 else '')
+        else:
+            print('{:c}'.format(ord(j)), end="\n" if i == len(str) - 1 else '')
