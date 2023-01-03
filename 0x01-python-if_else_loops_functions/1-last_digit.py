6381208e35d82f1f565caaa6e6d750a4662a063c
exuszee <exuszee@gmail.com>
2023-01-03 16:44:00 +0300

    fix 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,24 +2,24 @@
 import random
 number = random.randint(-10000, 10000)
 if len(str(abs(number))) == 1:
-    last_d = number
+    x = number
 elif number == 0:
-    last_d = 0
+    x = 0
 else:
-    last_d = int(str(number)[-1])
+    x = int(str(number)[-1])
 
 if number < 0:
-    last_d = last_d * -1
+    x = x * -1
 
-if last_d < 6:
-    if last_d != 0:
+if x < 6:
+    if x != 0:
         print(
-            f"Last digit of {number:d} is {last_d:d} and is less than 6 and is not 0")
-    if last_d == 0 and number == 0:
-        print(f"Last digit of {number:d} is {last_d:d} and is 0")
-    if last_d == 0 and number != 0:
+            f"Last digit of {number} is {x} and is less than 6 and not 0")
+    if x == 0 and number == 0:
+        print(f"Last digit of {number} is {x} and is 0")
+    if x == 0 and number != 0:
         print(
-            f"Last digit of {number:d} is {last_d:d} and is less than 6 and is 0")
+            f"Last digit of {number} is {x} and is less than 6 and is 0")
 
-elif last_d > 5:
-    print(f"Last digit of {number:d} is {last_d:d} and is greater than 5")
+elif x > 5:
+    print(f"Last digit of {number} is {x} and is greater than 5")
