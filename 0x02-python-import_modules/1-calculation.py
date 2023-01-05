79a41f99a06ded7d81749c3dab6f24b5f6b0abc8
exuszee <exuszee@gmail.com>
2023-01-05 15:16:50 +0300

    1. My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def my_function():
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
+
+
+if __name__ == "__main__":
+    import sys
+    my_function()
