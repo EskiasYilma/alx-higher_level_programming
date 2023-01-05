87d15f5ccca25c91f58ee365b19b83aee9b86316
exuszee <exuszee@gmail.com>
2023-01-05 16:04:13 +0300

    5. Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 from variable_load_5 import a
-
-
-def my_func():
-    print("".format(a))
-
-
 if __name__ == "__main__":
-    import sys
-    my_func()
+    print('{}'.format(a))
