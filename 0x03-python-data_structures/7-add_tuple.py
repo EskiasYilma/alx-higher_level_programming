02b60ffb1a582931bfc13097b128882d01040ea6
exuszee <exuszee@gmail.com>
2023-01-08 17:08:58 +0300

    fix 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    top_tup = ()
-    bot_tup = ()
-    if len(tuple_a) == len(tuple_b):
-        top_tup = tuple_a
-        bot_tup = tuple_b
-    else:
-        top_tup = tuple_a if len(tuple_a) > len(tuple_b) else tuple_b
-        bot_tup = tuple_a if len(tuple_a) < len(tuple_b) else tuple_b
-        for i in range(len(top_tup) - len(bot_tup)):
-            bot_tup = bot_tup + (0,)
-    res = []
-    for i in range(0, len(top_tup)):
-        res.append(top_tup[i] + bot_tup[i])
-    res = tuple(res)
-    return res
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
