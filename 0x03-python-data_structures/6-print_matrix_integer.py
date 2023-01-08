d32dc18acc4d5a21e47e1ebb92e7bb7638e345ac
exuszee <exuszee@gmail.com>
2023-01-08 17:04:54 +0300

    fix 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            # for j, k in enumerate(i):
             print(matrix[i][j], end='' if j == len(matrix[0]) - 1 else " ")
         print()
