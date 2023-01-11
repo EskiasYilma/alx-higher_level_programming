e673ec1923f3d8b48214e33f640179bf2c7c4c46
Eskias Yilma <eskiasyt@gmail.com>
2023-01-11 12:20:19 +0300

    fix 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,11 @@
 def best_score(a_dictionary):
     if a_dictionary:
         best_val = int(list(a_dictionary.values())[0])
+        best_key = list(a_dictionary.keys())[0]
         for i, j in a_dictionary.items():
             if j > best_val:
                 best_val = j
+                best_key = i
     else:
-        best_val = None
-    return best_val
+        best_key = None
+    return best_key
