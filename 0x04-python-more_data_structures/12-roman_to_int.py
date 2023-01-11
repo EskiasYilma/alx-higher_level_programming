bc38b97c75a049967f7bd32da487ed5c2df45608
exuszee <exuszee@gmail.com>
2023-01-11 15:48:54 +0300

    12. Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rom_to_int_dict = {'I': 1, 'V': 5, 'X': 10,
+                       'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    x = 0
+    i = 0
+    while (i < len(roman_string)):
+        temp1 = rom_to_int_dict[roman_string[i]]
+        if (i + 1 < len(roman_string)):
+            temp2 = rom_to_int_dict[roman_string[i + 1]]
+            if (temp1 >= temp2):
+                x += temp1
+                i += 1
+            else:
+                x += temp2 - temp1
+                i += 2
+        else:
+            x += temp1
+            i += 1
+    return x
