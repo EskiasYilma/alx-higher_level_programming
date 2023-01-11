5f8f72256b33f261d0cf301998c2a0a47db0da4a
Eskias Yilma <eskiasyt@gmail.com>
2023-01-11 12:03:11 +0300

    10-11

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best_val = list(a_dictionary.values())[0]
+    for i, j in a_dictionary.items():
+        if j > best_val:
+            best_val = j
+
+    return best_val
