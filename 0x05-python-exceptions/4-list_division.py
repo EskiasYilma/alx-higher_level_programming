321259644ccc4cb12a5dd30955d96825186603aa
exuszee <exuszee@gmail.com>
2023-01-23 14:49:29 +0300

    q-4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,12 @@ def list_division(my_list_1, my_list_2, list_length):
                 div = 0
                 div_list.append(div)
                 continue
-            if type(my_list_1[i]) not in [int, float] or type(my_list_2[i]) not in [int, float]:
+            if type(my_list_1[i]) not in [int, float]:
+                print("wrong type")
+                div = 0
+                div_list.append(div)
+                continue
+            if type(my_list_2[i]) not in [int, float]:
                 print("wrong type")
                 div = 0
                 div_list.append(div)
@@ -32,3 +37,16 @@ def list_division(my_list_1, my_list_2, list_length):
             pass
 
     return div_list
+
+
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
