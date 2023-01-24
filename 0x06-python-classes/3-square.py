46d42f2af278d905920069f85b99b5706287675f
exuszee <exuszee@gmail.com>
2023-01-24 15:56:06 +0300

    q3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Module Docstring
+"""
+
+
+class Square:
+    """
+    Class Docstring
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize class Docstring
+        """
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Public instance method Docstring
+        """
+        return self.__size * self.__size
