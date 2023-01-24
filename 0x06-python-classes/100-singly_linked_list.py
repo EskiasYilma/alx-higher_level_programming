7c37d1e0c8f88db59814941d638b3dff467c18f9
exuszee <exuszee@gmail.com>
2023-01-24 20:11:54 +0300

    fix 7

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -94,18 +94,3 @@ class SinglyLinkedList:
             if h:
                 lst += "\n"
         return lst
-
-
-sll = SinglyLinkedList()
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-print(sll)
