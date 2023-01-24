d2ddc5ae0bc1a82d35ff4d5f51c26d9844d4376e
exuszee <exuszee@gmail.com>
2023-01-24 20:40:41 +0300

    10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+import math
+"""
+Module Docstring
+"""
+
+
+class MagicClass:
+    """
+    Class Docstring
+    """
+
+    def __init__(self, radius=0):
+        """
+        Initialize class Docstring
+        """
+        self.__radius = 0
+        if (type(radius) is not int) or (type(radius) is not float):
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    @property
+    def radius(self):
+        """
+        Property method Docstring
+        """
+        return self.__radius
+
+    def area(self):
+        """
+        Public instance method Docstring
+        """
+        return math.pi * self._radius * self.__radius
+
+    def circumference(self):
+        """
+        Public instance method Docstring
+        """
+        return 2 * math.pi * self.__radius
