fb003a4d46621c4324eea789dd165ae0b8070ff9
exuszee <exuszee@gmail.com>
2023-01-30 20:18:05 +0300

    q6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,26 +15,38 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_empty(self):
         """
-        test_empty Function Docstring
-        Tests for empty list
-        """
-        self.assertIsNone(max_integer([]))
+        test_empty
 
-    def test_integers(self):
+        Tests for max_integer using an empty list
         """
-        test_integers Function Docstring
-        Tests for max integer for positive numbers
+        self.assertIsNone(max_integer([]), None)
+
+    def test_positives(self):
+        """
+        test_positives
+
+        Tests for max_integer using a list with positive integers
         """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([4, 3, 2, 1]), 4)
 
     def test_negatives(self):
         """
-        test_negatives Function Docstring
-        Tests for max integer for negetive numbers
+        test_negatives
+
+        Tests for max_integer using a list with negative integers
         """
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
+
+    def test_positive_and_negative(self):
+        """
+        test_positive_and_negative
+
+        Tests for max_integer using a list with both positive and negative integers
+        """
+        self.assertEqual(max_integer([-1, -2, -3, 4, 5]), 5)
+        self.assertEqual(max_integer([-5, 4, 3, 2, 1]), 4)
 
 
 if __name__ == '__main__':
