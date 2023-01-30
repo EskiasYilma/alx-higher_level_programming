414dd49be9b0e98cb875ada5e06ead733835524b
exuszee <exuszee@gmail.com>
2023-01-30 16:16:32 +0300

    q2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -62,6 +62,7 @@ class Rectangle:
         """
         Public instance method Docstring
         """
-        if width == 0 or height == 0:
+        if (self.__width == 0) or (self.__height == 0):
             return 0
-        return 2 * (self.__width + self.__height)
+        else:
+            return 2 * (self.__width + self.__height)
