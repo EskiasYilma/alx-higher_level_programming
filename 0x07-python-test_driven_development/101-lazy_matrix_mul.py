d56a042dcbb1355f6e145d28e393522b57ebea21
exuszee <exuszee@gmail.com>
2023-01-30 21:37:15 +0300

    q101 typo fix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,14 +2,14 @@
 """
 Module Docstring for lazy_matrix_mul(m_a, m_b)
 
-Returns a multiplication of 2 matrices
+Returns multiplication of 2 matrices m_a and m_b by using the module NumPy
 """
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """
-    Function to return the multiplication of 2 matrices m_a and m_b
+    Function to return the multiplication of 2 matrices m_a and m_b by using the module NumPy
     Checks:
         - if matrices are formatted properly
         - if matrices contain only floats and ints and are not empty
