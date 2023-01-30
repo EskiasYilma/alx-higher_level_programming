feb0e3838b953cddd71bf7e2a16b38697cb8ed96
exuszee <exuszee@gmail.com>
2023-01-30 16:01:15 +0300

    q0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Module DOcstring
+"""
+
+class Rectangle:
+    """
+    Class Docstring
+    """
+    def __init__(self):
+        """
+        Init Docstring
+        """
+        pass
