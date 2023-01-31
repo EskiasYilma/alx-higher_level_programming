e997496658cb3f513e4b42acd20ea4754fa894d3
gHost <eskiasyt@gmail.com>
2023-01-31 11:56:53 +0300

    100 typo

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -15,19 +15,19 @@ def matrix_mul(m_a, m_b):
         - if martices are multipliable
         - if martices are of the same size
     """
-    if not m_a or any(not row for row in m_a):
-        raise ValueError("m_a can't be empty")
-    if not m_b or any(not row for row in m_b):
-        raise ValueError("m_b can't be empty")
-    if type(m_a) is not list:
+
+    if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    if type(m_b) is not list:
+    if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-
+    if not m_a or any(not row for row in m_a):
+        raise ValueError("m_a can't be empty")
+    if not m_b or any(not row for row in m_b):
+        raise ValueError("m_b can't be empty")
     if not all(all(isinstance(i, (int, float)) for i in row) for row in m_a):
         raise TypeError("m_a should contain only integers or floats")
     if not all(all(isinstance(i, (int, float)) for i in row) for row in m_b):
