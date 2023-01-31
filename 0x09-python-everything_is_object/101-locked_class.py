0e9c268e0ee2bd51c2fb9aad2a79ad6c3bc3dbba
exuszee <exuszee@gmail.com>
2023-01-31 18:03:27 +0300

    q101 slots worked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,7 +7,6 @@ Module Docstring
 class LockedClass:
     """
     Only allows instatiation of an attribute named first_name
-    The special attribute __slots__ allows to explicitly state which instance attributes are expected for object instances to have.
     """
 
     __slots__ = ["first_name"]
