42415554749077a167ce2db4fa677316c1c43097
exuszee <exuszee@gmail.com>
2023-01-31 17:14:50 +0300

    q101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Module Docstring
+"""
+
+
+class LockedClass:
+    """
+    LockedClass Docstring
+    """
+
+    def __setattr__(self, key, value):
+        """
+        Fucntion to check and validate if the passed instance is first_name
+        """
+        if key != 'first_name':
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{}'".format(key))
+        super().__setattr__(key, value)
