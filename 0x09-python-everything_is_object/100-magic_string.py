ef285faf34caca0ac6697ac6bfc21a18c16627d5
exuszee <exuszee@gmail.com>
2023-01-31 16:29:45 +0300

    q100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,7 +2,3 @@
 def magic_string():
     magic_string.cnt = getattr(magic_string, 'cnt', 0) + 1
     return ", ".join(["BestSchool" for x in range(magic_string.cnt)])
-
-
-for i in range(10):
-    print(magic_string())
