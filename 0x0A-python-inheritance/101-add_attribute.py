090f3a6ee31750ce951c44aca455f1c51b0379b5
exuszee <exuszee@gmail.com>
2023-02-06 19:38:56 +0300

    q101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,3 +2,27 @@
 """
 Module Docstring
 """
+
+
+def add_attribute(object, name, attr):
+    test_attr = getattr(object, "__doc__", None)
+    if test_attr == None:
+        setattr(object, name, attr)
+    else:
+        raise TypeError("can't add new attribute")
+
+
+class MyClass():
+    pass
+
+
+mc = MyClass()
+add_attribute(mc, "name", "John")
+print(mc.name)
+
+try:
+    a = "My String"
+    add_attribute(a, "name", "Bob")
+    print(a.name)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
