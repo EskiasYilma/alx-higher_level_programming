e36d31b5b411a4a0a8177e31e5f2241578f7da5d
exuszee <exuszee@gmail.com>
2023-02-06 19:27:41 +0300

    q12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,3 +2,15 @@
 """
 Module Docstring
 """
+
+
+class MyInt(int):
+    """
+    MyInt the Rebel
+    """
+
+    def __ne__(self, comp):
+        return (self - comp == 0)
+
+    def __eq__(self, comp):
+        return (self - comp != 0)
