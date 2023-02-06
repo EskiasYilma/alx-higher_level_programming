9e4cddd3eadd22051170a881700f21c3a2d188cd
exuszee <exuszee@gmail.com>
2023-02-06 19:40:37 +0300

    q101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,8 +5,10 @@ Module Docstring
 
 
 def add_attribute(object, name, attr):
-    test_attr = getattr(object, "__doc__", None)
-    if test_attr == None:
+    """
+    add_attribute Function
+    """
+    if getattr(object, "__doc__", None) is None:
         setattr(object, name, attr)
     else:
         raise TypeError("can't add new attribute")
