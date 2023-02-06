df0dd93b9e0aca12af7f5d5d4bbf057c33760cd0
exuszee <exuszee@gmail.com>
2023-02-06 16:49:26 +0300

    q4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,3 +2,15 @@
 """
 Module Docstring
 """
+
+
+def inherits_from(obj, a_class):
+    """
+    inherits_from Docstring
+    Returns True if the object is an instance of a\
+    class that inherited (directly or indirectly)\
+    from the specified class ; otherwise False
+    """
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    return False
