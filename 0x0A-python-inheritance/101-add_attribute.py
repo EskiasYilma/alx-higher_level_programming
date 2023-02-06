4955d373f5dbb293770945f540e8b316091e73bb
exuszee <exuszee@gmail.com>
2023-02-06 19:41:10 +0300

    q101-fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,19 +12,3 @@ def add_attribute(object, name, attr):
         setattr(object, name, attr)
     else:
         raise TypeError("can't add new attribute")
-
-
-class MyClass():
-    pass
-
-
-mc = MyClass()
-add_attribute(mc, "name", "John")
-print(mc.name)
-
-try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
