f79416e28f18dfa77e8b363c40ea7bde9d20b6bc
exuszee <exuszee@gmail.com>
2023-02-07 18:20:41 +0300

    q12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,3 +2,22 @@
 """
 Module Docstring
 """
+
+
+def pascal_triangle(n):
+    """
+    pascal_triangle
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            row = [1]
+            for j in range(i - 1):
+                row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+            row.append(1)
+            triangle.append(row)
+    return triangle
