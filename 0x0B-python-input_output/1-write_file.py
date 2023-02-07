c30cc6b34de5918e7d4a2515564aaaaac4b5a22d
exuszee <exuszee@gmail.com>
2023-02-07 15:49:57 +0300

    q1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,3 +2,11 @@
 """
 Module Docstring
 """
+
+
+def write_file(filename="", text=""):
+    """
+    write_file
+    """
+    with open(filename, 'w', encoding="UTF8") as f:
+        return f.write(text)
