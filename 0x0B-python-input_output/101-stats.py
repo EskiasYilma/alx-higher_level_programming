aec5bb3482e70e06ce444299ece08dd9bb0cfd6c
exuszee <exuszee@gmail.com>
2023-02-07 19:39:17 +0300

    q101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,42 +3,56 @@
 Module Docstring
 """
 import sys
-import signal
+
+
+def tokenize(line):
+    """
+    tokenize docstring
+    """
+    elements = line.split()
+    status_code = int(elements[-2])
+    file_size = int(elements[-1])
+    return status_code, file_size
+
+
+def update_data(metrics, status_code, file_size):
+    """
+    update_data docstring
+    """
+    metrics['ttl_size'] += file_size
+    if status_code in metrics['stat_codes']:
+        metrics['stat_codes'][status_code] += 1
+    else:
+        metrics['stat_codes'][status_code] = 1
+
+
+def print_metrics(metrics):
+    """
+    print_metrics docstring
+    """
+    sys.stdout.write("File size: {}\n".format(metrics['ttl_size']))
+    for status_code in sorted(metrics['stat_codes'].keys()):
+        sys.stdout.write("{}: {}\n".format(status_code,
+                                           metrics['stat_codes'][status_code]))
+
+
+def main():
+    """
+    main docstring
+    """
+    metrics = {'ttl_size': 0, 'stat_codes': {}}
+    lines_read = 0
+    try:
+        for line in sys.stdin:
+            status_code, file_size = tokenize(line)
+            update_data(metrics, status_code, file_size)
+            lines_read += 1
+            if lines_read % 10 == 0:
+                print_metrics(metrics)
+    except KeyboardInterrupt:
+        print_metrics(metrics)
+        raise
+
 
 if __name__ == '__main__':
-
-    def signal_handler(signal, frame):
-        """
-        signal_handler
-        """
-        print("File size: {}".format(file_size))
-        for key, value in sorted(status_codes.items()):
-            print("{}: {}".format(key, value))
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, signal_handler)
-
-    status_codes = {}
-    file_size = 0
-    counter = 0
-
-    while True:
-        line = sys.stdin.readline()
-        if not line:
-            break
-        counter += 1
-        try:
-            items = line.split(" ")
-            status = int(items[-2])
-            size = int(items[-1])
-            if status not in status_codes:
-                status_codes[status] = 0
-            status_codes[status] += 1
-            file_size += size
-        except Exception:
-            continue
-        if counter == 10:
-            print("File size: {}".format(file_size))
-            for key, value in sorted(status_codes.items()):
-                print("{}: {}".format(key, value))
-            counter = 0
+    main()
