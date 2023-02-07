ced8f79a4cf583c93c4c1e44bb585c2c3cb0a885
exuszee <exuszee@gmail.com>
2023-02-07 17:39:11 +0300

    q8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,3 +2,11 @@
 """
 Module Docstring
 """
+
+
+def class_to_json(obj):
+    """
+    class_to_json
+    """
+    return {key: value for key, value in obj.__dict__.items()
+            if type(value) in [list, dict, str, int, bool]}
