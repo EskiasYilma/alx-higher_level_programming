c12c04d5b27f61e09cc358a7d1e496cdcb6a8821
exuszee <exuszee@gmail.com>
2023-02-07 19:01:06 +0300

    q101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,3 +2,56 @@
 """
 Module Docstring
 """
+
+
+def tokenize(line):
+    """
+    tokenize docstring
+    """
+    elements = line.split()
+    status_code = int(elements[-2])
+    file_size = int(elements[-1])
+    return status_code, file_size
+
+
+def update_data(metrics, status_code, file_size):
+    """
+    update_data docstring
+    """
+    metrics['ttl_size'] += file_size
+    if status_code in metrics['stat_codes']:
+        metrics['stat_codes'][status_code] += 1
+    else:
+        metrics['stat_codes'][status_code] = 1
+
+
+def print_metrics(metrics):
+    """
+    print_metrics docstring
+    """
+    print("File size: {}".format(metrics['ttl_size']))
+    for status_code in sorted(metrics['stat_codes'].keys()):
+        print("{}: {}".format(status_code,
+                              metrics['stat_codes'][status_code]))
+
+
+def main():
+    """
+    main docstring
+    """
+    metrics = {'ttl_size': 0, 'stat_codes': {}}
+    lines_read = 0
+    try:
+        while True:
+            line = input()
+            status_code, file_size = tokenize(line)
+            update_data(metrics, status_code, file_size)
+            lines_read += 1
+            if lines_read % 10 == 0:
+                print_metrics(metrics)
+    except KeyboardInterrupt:
+        print_metrics(metrics)
+
+
+if __name__ == '__main__':
+    main()
