e9a05818425abde3bf23ea0554ea38d0f631bd94
exuszee <exuszee@gmail.com>
2023-02-07 15:30:08 +0300

    q0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,5 @@ def read_file(filename=""):
     read_file docstring
     """
     with open(filename, encoding="UTF8") as f:
-        print("".join(f.readlines()))
+        for line in f:
+            print(line, end='')
