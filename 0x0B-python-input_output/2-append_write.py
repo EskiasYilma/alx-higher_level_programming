ba59b1a42b9e7d05f8f218fcdbe5f5abea2db18f
exuszee <exuszee@gmail.com>
2023-02-07 15:51:14 +0300

    q1

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,3 +2,11 @@
 """
 Module Docstring
 """
+
+
+def append_write(filename="", text=""):
+    """
+    append_write
+    """
+    with open(filename, 'a', encoding="UTF8") as f:
+        return f.write(text)
