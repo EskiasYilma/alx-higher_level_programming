5364f409d4207282fac87ffa6e37ea8a812d60ce
exuszee <exuszee@gmail.com>
2023-02-07 17:27:29 +0300

    q7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,6 @@
 Module Docstring
 """
 import sys
-import os
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
@@ -12,17 +11,14 @@ def add_to_list(args):
     """
     add_to_list
     """
-    if os.path.isfile("add_item.json"):
-        if os.path.getsize('add_item.json') == 0:
-            save_to_json_file([], "add_item.json")
-        else:
-            data = load_from_json_file("add_item.json")
-            if args:
-                for i in args[1:]:
-                    data.append(i)
-                save_to_json_file(data, "add_item.json")
-    else:
-        save_to_json_file([], "add_item.json")
+    try:
+        data = load_from_json_file("add_item.json")
+    except Exception:
+        pass
+    data = []
+    for i in args[1:]:
+        data.append(i)
+    save_to_json_file(data, "add_item.json")
 
 
 if __name__ == "__main__":
