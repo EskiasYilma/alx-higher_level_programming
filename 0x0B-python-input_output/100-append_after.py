e51ad06c597f2f9660b17b7eaa0c0c91e46c07c8
exuszee <exuszee@gmail.com>
2023-02-07 18:35:24 +0300

    q100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,3 +2,14 @@
 """
 Module Docstring
 """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    data = ""
+    with open(filename, "r") as f:
+        for line in f:
+            data += line
+            if search_string in line:
+                data += new_string
+    with open(filename, "w") as f:
+        f.write(data)
