1aff8738a691e6bc9b91839e374ece8d3ae449d2
exuszee <exuszee@gmail.com>
2023-03-15 17:16:16 +0300

    19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,4 @@
 -- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-
 --     Each record should display: tv_shows.title - rating sum
 --     Results must be sorted in descending order by the rating
 --     You can use only one SELECT statement
