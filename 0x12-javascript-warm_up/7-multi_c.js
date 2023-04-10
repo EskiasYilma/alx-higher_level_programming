ee6e44e86b5808e34314b3acd8852030db2e8246
exuszee <exuszee@gmail.com>
2023-04-10 17:38:34 +0300

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+/*
+prints x times “C is fun”
+*/
+
+const process = require('process');
+
+if (String(Number(process.argv[2])) === 'NaN') {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
