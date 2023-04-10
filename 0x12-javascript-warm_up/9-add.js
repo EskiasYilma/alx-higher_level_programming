981166907d2fce0dbe509e16e00fc45794e8f0f6
exuszee <exuszee@gmail.com>
2023-04-10 18:13:33 +0300

    9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+/*
+prints a square
+*/
+
+const process = require('process');
+
+function add (a, b) {
+  return a + b;
+}
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
