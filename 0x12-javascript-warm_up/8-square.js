5d57a6bca96a41365407f40ab4781c138915a29e
exuszee <exuszee@gmail.com>
2023-04-10 18:06:06 +0300

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+
+/*
+prints a square
+*/
+
+const process = require('process');
+
+let freq = '';
+
+if (String(Number(process.argv[2])) === 'NaN') {
+  console.log('Missing size');
+} else {
+  if (Number(process.argv[2]) > 0) {
+    for (let i = 0; i < Number(process.argv[2]); i++) {
+      for (let i = 0; i < Number(process.argv[2]); i++) {
+        freq += 'X';
+      }
+      if (i === Number(process.argv[2]) - 1) {
+        break;
+      } else {
+        freq += '\n';
+      }
+    }
+    console.log(freq);
+  }
+}
