7e91a94de7e761a5444fcc6aca5946d5d7f397e6
exuszee <exuszee@gmail.com>
2023-04-10 18:58:08 +0300

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,19 +11,19 @@ const array = [];
 if (String(Number(process.argv[2])) === 'NaN' || process.argv.length - 2 === 1) {
   console.log(0);
 } else {
-    for (let i = 0; i < (process.argv.length - 2); i++) {
+  for (let i = 0; i < (process.argv.length - 2); i++) {
     array.push(Number(process.argv[i + 2]));
-    }
-    let x = 0;
+  }
+  let x = 0;
 
-    for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < i; j++) {
-        if (array[i] < array[j]) {
-          x = array[i];
-          array[i] = array[j];
-          array[j] = x;
-        }
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (array[i] < array[j]) {
+        x = array[i];
+        array[i] = array[j];
+        array[j] = x;
       }
     }
-    console.log(array[1]);
+  }
+  console.log(array[1]);
 }
