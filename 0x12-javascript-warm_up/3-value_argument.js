8f62f77ae3335ed9f4d5003f48902d8ab1a0ac2f
exuszee <exuszee@gmail.com>
2023-04-10 17:02:29 +0300

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+/*
+prints a message depending of the number of arguments passed
+*/
+
+const process = require('process');
+
+if (process.argv.length - 2 >= 1) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
+}
