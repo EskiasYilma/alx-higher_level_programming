de95fd1600839f8492ef7d19368b3c2918df3d07
exuszee <exuszee@gmail.com>
2023-04-10 20:33:29 +0300

    15

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+/*
+returns the addition of 2 integers.
+*/
+
+module.exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
