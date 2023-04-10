2388e0b93476ba09a47e405edffb824170ec650c
exuszee <exuszee@gmail.com>
2023-04-10 17:07:34 +0300

    4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+/*
+prints a message depending of the number of arguments passed
+*/
+
+const process = require('process');
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
