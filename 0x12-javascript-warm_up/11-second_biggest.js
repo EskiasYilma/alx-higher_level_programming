470e0378e35c3f50ff8baceeb60dd8d7f6c19b87
exuszee <exuszee@gmail.com>
2023-04-10 18:42:44 +0300

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+/*
+searches the second biggest integer in the list of arguments.
+*/
+
+const process = require('process');
+
+const array = [];
+
+if (String(Number(process.argv[2])) === 'NaN' || process.argv.length - 2 === 1) {
+  console.log(0);
+} else {
+  for (let i = 0; i < (process.argv.length - 2); i++) {
+    array.push(Number(process.argv[i + 2]));
+  }
+  console.log(array.sort()[1]);
+}
