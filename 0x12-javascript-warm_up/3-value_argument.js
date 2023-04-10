27b60df27c87773a2628c0721afa00bef79306b6
exuszee <exuszee@gmail.com>
2023-04-10 17:04:44 +0300

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -6,7 +6,7 @@ prints a message depending of the number of arguments passed
 
 const process = require('process');
 
-if (process.argv[3]) {
+if (process.argv[2]) {
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
