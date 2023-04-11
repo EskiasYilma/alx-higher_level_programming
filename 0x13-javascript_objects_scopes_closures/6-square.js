70fffc8bfcf66122ccfbb198f866212ae92dc113
exuszee <exuszee@gmail.com>
2023-04-11 16:11:02 +0300

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Square = require('./5-square');
+const Sq = require('./5-square');
 
-class Square extends Square {
+class Square extends Sq {
   constructor (size) {
     super(size, size);
   }
