fe61059427c4c07133730a160a0f2fc5046962a9
exuszee <exuszee@gmail.com>
2023-04-11 16:06:02 +0300

    6

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,12 +4,7 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super();
-    if (size <= 0 || typeof size !== 'number') {
-      return;
-    }
-    this.width = size;
-    this.height = size;
+    super(size, size);
   }
 }
 
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    if (c === undefined) {
+        c = 'X';
+    }
+
+    let freq = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        freq += c;
+      }
+      if (i === this.height - 1) {
+        break;
+      } else {
+        freq += '\n';
+      }
+    }
+    console.log(freq);
+  }
+
+}
+
+module.exports = Square;
