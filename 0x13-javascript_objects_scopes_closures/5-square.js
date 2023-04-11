99f8447fb27eb6be117d4f32c6a3e6124b2991ce
exuszee <exuszee@gmail.com>
2023-04-11 15:54:47 +0300

    5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,6 +5,9 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super();
+    if (size <= 0 || typeof size !== 'number') {
+      return;
+    }
     this.width = size;
     this.height = size;
   }
