38136ba970fcfdb2493a03bb465179aa1b8be9b2
exuszee <exuszee@gmail.com>
2023-04-11 18:03:14 +0300

    13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+const f1 = fs.readFileSync('fileA', 'utf8');
+const f2 = fs.readFileSync('fileB', 'utf8');
+const f3 = f1 + f2;
+
+fs.writeFileSync('fileC', f3);
