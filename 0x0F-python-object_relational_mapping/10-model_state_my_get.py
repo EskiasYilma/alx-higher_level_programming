948ae589e34e02943fe9e3ba2b334d1bb796aafc
exuszee <exuszee@gmail.com>
2023-04-15 19:37:15 +0300

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A script that lists all State objects that contain the letter
-a from the database hbtn_0e_6_usa
+A script that prints the State object with the name \
+passed as argument from the database hbtn_0e_6_usa
 """
 
 if __name__ == "__main__":
