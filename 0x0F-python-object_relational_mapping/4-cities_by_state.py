324f3a347dde7fe40ceb98216e50bcb3d3508bd3
exuszee <exuszee@gmail.com>
2023-04-15 18:19:23 +0300

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
 """
-Module Docstring
+A script that lists all cities from the database hbtn_0e_4_usa
 """
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=mysql_username, passwd=mysql_password,
+                         db=database_name)
+
+    cursor = db.cursor()
+    my_query = "SELECT cities.id, cities.name, states.name FROM cities, \
+                states WHERE cities.state_id = states.id GROUP BY cities.name \
+                ORDER BY cities.id ASC"
+    cursor.execute(my_query)
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    db.close()
