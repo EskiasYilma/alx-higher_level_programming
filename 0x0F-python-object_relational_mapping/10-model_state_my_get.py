adfd2ab27b7a61b9a37fed7617d9576d2d2d44b2
exuszee <exuszee@gmail.com>
2023-04-15 19:30:21 +0300

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python3
 """
-Module Docstring
+A script that lists all State objects that contain the letter
+a from the database hbtn_0e_6_usa
 """
+
+if __name__ == "__main__":
+
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    import sys
+    from sqlalchemy.orm import sessionmaker
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (mysql_username,
+                            mysql_password,
+                            database_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).filter_by(
+                  name=state_name_searched).first()
+
+    print(first_state.id if first_state else "Not Found")
+
+    session.close()
