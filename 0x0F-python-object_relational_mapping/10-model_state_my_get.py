5ede82085c649647ff5ba7a0b1016d578e25ae63
exuszee <exuszee@gmail.com>
2023-04-16 19:31:04 +0300

    10 fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,12 +15,15 @@ if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-    state_name_searched = sys.argv[4]
+    state_name_searched = sys.argv[4].split("'")[0]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (mysql_username,
-                            mysql_password,
-                            database_name), pool_pre_ping=True)
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                               (mysql_username,
+                                mysql_password,
+                                database_name), pool_pre_ping=True)
+    except Exception:
+        exit(1)
 
     query = text("SELECT * FROM states WHERE name = :state_name")
     result = engine.execute(query, state_name=state_name_searched)
