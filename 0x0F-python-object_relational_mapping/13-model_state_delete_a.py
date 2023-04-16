03096334d9a41afee572b237bbe612c69860b2ac
exuszee <exuszee@gmail.com>
2023-04-16 17:53:16 +0300

    13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A script that adds the State object “Louisiana” \
-to the database hbtn_0e_6_usa
+A script that deletes all State objects with a \
+name containing the letter a from the database hbtn_0e_6_usa
 """
 
 if __name__ == "__main__":
