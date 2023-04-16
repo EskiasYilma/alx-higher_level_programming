99395a142247ab1d22a06af86419e6255c9d154f
exuszee <exuszee@gmail.com>
2023-04-16 17:22:29 +0300

    11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
 """
-Module Docstring
+A script that adds the State object “Louisiana” \
+to the database hbtn_0e_6_usa
 """
+
+if __name__ == "__main__":
+
+    from sqlalchemy import create_engine, insert
+    from model_state import Base, State
+    import sys
+    from sqlalchemy.orm import sessionmaker
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (mysql_username,
+                            mysql_password,
+                            database_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    loui = State(name="Louisiana")
+    session.add(loui)
+    session.commit()
+    session.flush()
+    print(loui.id)
