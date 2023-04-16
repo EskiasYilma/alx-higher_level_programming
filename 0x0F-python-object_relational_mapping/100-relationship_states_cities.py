946c79ae8c8ccecd0d17eebb56d83158331607dc
exuszee <exuszee@gmail.com>
2023-04-16 18:45:15 +0300

    100, reln_city, reln_state

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,4 +1,35 @@
 #!/usr/bin/python3
 """
-Module Docstring
+A Script that creates the State “California” with the City “San Francisco” \
+from the database hbtn_0e_100_usa
 """
+
+if __name__ == "__main__":
+
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+    import sys
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (mysql_username,
+                            mysql_password,
+                            database_name), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    add_state = State(name="California")
+    add_city = City(name="San Francisco", state=add_state)
+
+    session.add(add_state)
+    session.add(add_city)
+    session.commit()
+
+    session.close()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -9,6 +9,7 @@ class definition of a State and an instance Base = declarative_base():
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -23,8 +24,14 @@ class State(Base):
         - can’t be null and is a primary key
         - class attribute name that represents a column of a string
         with maximum 128 characters and can’t be null
+        - class attribute cities that represents a relationship \
+         with the class City. If the State object is deleted, all\
+         linked City objects must be automatically deleted. Also, \
+         the reference from a City object to his State should be \
+         named state
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True,
                 nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
