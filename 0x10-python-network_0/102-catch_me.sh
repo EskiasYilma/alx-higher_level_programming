3c26ccf799aa1a1ec894af303cd76a295f69e839
exuszee <exuszee@gmail.com>
2023-04-27 18:38:27 +0300

    9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# A script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -s -X PUT -d "X-School-User-Id=98" 0.0.0.0:5000/catch_me --output /dev/null --location
