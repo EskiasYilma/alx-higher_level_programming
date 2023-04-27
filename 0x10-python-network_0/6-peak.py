e155034310e0faa42abc54d7e2174dc37b7ff28e
exuszee <exuszee@gmail.com>
2023-04-27 18:01:22 +0300

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,10 +3,11 @@
 A function that finds a peak in a list of unsorted integers.
 """
 
+
 def find_peak(list_of_integers):
     """
     A function that finds a peak in a list of unsorted integers.
     """
     if len(list_of_integers) != 0:
         list_of_integers.sort()
-        return(list_of_integers[-1])
+        return (list_of_integers[-1])
