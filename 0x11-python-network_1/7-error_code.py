6546c64fca81b6ff6c3a1494ae0301942119159e
exuszee <exuszee@gmail.com>
2023-04-28 16:50:42 +0300

    6

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+A script that takes in a URL and an email address, sends a \
+POST request to the passed URL with the email as a parameter\
+, and finally displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.post(str(sys.argv[1]))
+    if int(r.status_code) >= 400:
+        print("Error code: {}".format(r.status_code))
