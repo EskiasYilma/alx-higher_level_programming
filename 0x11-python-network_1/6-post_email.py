9bf5787779eec014de943cd1979b43cc961aed57
exuszee <exuszee@gmail.com>
2023-04-28 16:21:17 +0300

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+A script that takes in a URL and an email address, sends a \
+POST request to the passed URL with the email as a parameter\
+, and finally displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    data = {'email': str(sys.argv[2])}
+    r = requests.post(str(sys.argv[1]), data=data)
+    print(r.content)
