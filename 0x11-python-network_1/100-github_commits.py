7cd403e3edfc7c82b8161e729b40bbea7f2260e5
exuszee <exuszee@gmail.com>
2023-04-28 17:34:44 +0300

    10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """
 A script that takes 2 arguments in order to solve this challenge.
+
+    The first argument will be the repository name
+    The second argument will be the owner name
+    You must use the packages requests and sys
+    You are not allowed to import packages other than requests and sys
+    You don’t need to check arguments passed to the script (number or type)
+
 """
 
 if __name__ == "__main__":
