5543b2a9dff9c35e6494a32977d851e324393f80
exuszee <exuszee@gmail.com>
2023-04-28 16:18:39 +0300

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+A script that takes in a URL, sends a request to the URL \
+and displays the value of the variable X-Request-Id in \
+the response header
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get(str(sys.argv[1]))
+    print(r.headers.get('X-Request-Id'))
