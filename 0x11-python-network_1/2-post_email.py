a6bac3ad618682eeeab3783082ba910d878aeb96
exuszee <exuszee@gmail.com>
2023-04-28 15:59:12 +0300

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     from urllib import request, parse
     import sys
 
-    data = parse.urlencode([('email':str(sys.argv[2]))])
-    with request.Request(str(sys.argv[1]), data=data) as u:
-        resp = request.urlopen(u)
-        print(resp.read().decode('utf-8'))
+    data = parse.urlencode({'email': str(sys.argv[2])}).encode('utf-8')
+    r = request.Request(str(sys.argv[1]), data=data)
+    with request.urlopen(r) as u:
+        print(u.read().decode('utf-8'))
