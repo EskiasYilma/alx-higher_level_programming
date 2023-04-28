eda7c624d76797a59264ab8060f1c8a55bc1998d
exuszee <exuszee@gmail.com>
2023-04-28 16:51:48 +0300

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.post(str(sys.argv[1]))
+    r = requests.get(str(sys.argv[1]))
     if int(r.status_code) >= 400:
         print("Error code: {}".format(r.status_code))
