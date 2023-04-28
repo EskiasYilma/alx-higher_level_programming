e623774943319e28b739e263d8f0bd2b9697a3ab
exuszee <exuszee@gmail.com>
2023-04-28 17:24:00 +0300

    9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+A script that takes your GitHub credentials (username and \
+password) and uses the GitHub API to display your id.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get("https://api.github.com/user",
+                     auth=(sys.argv[1], sys.argv[2]))
+    try:
+        print(r.json()['id'])
+    except Exception:
+        print("None")
