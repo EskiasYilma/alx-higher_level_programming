aec81436f917577d66b8b24db772fb9917c180b4
exuszee <exuszee@gmail.com>
2023-04-28 17:18:25 +0300

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+A script that takes in a letter and sends a POST request to\
+ http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    try:
+        data = {"q": str(sys.argv[1])}
+    except Exception:
+        data = {"q": ""}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        if len(r.json()) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.json()['id'], r.json()['name']))
+    except Exception:
+        print("Not a valid JSON")
