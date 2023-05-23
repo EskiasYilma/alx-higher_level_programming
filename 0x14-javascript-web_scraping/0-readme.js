5d1099ad5e9ee34e78d05f6ec852b9b12efb5c66
exuszee <exuszee@gmail.com>
2023-05-23 14:38:53 +0300

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,10 +5,12 @@ reads and prints the content of a file.
 
 const file = require('fs');
 
-file.readFile(process.argv[2], 'utf-8', function (f, err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(f);
-  }
-});
+if (process.argv[2]) {
+  file.readFile(process.argv[2], 'utf-8', function (f, err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(f);
+    }
+  });
+}
