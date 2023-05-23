3f4b6a3d39e5df0a1e6783292a0144eebb004e2d
exuszee <exuszee@gmail.com>
2023-05-23 19:13:18 +0300

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const req = require('request');
+let count = 0;
+
+req(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+
+  const results = JSON.parse(body).results;
+  for (let i = 0; i < results.length; i++) {
+    if (results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  }
+  console.log(count);
+});
