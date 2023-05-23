c572d55ea748b88591a12b6c39c47e4a08a243bf
exuszee <exuszee@gmail.com>
2023-05-23 20:54:23 +0300

    6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const req = require('request');
+
+req(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const results = JSON.parse(body);
+  const userdict = {};
+  for (let i = 1; i <= 10; i++) {
+    let count = 0;
+    for (let j = 0; j < results.length; j++) {
+      if (results[j].userId === i) {
+        if (results[j].completed === true) {
+          count++;
+        }
+      }
+    }
+    userdict[i] = count;
+  }
+  console.log(userdict);
+});
