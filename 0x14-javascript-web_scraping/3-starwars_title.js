4b9132a90db622f13365ade2ecbb3602829ee15e
exuszee <exuszee@gmail.com>
2023-05-23 15:06:01 +0300

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const req = require('request');
+
+req('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(JSON.parse(body).title);
+});
