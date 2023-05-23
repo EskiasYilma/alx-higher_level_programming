331a7d2ab553ab3ac870bfbc4c090972e63fdfb3
exuszee <exuszee@gmail.com>
2023-05-23 19:56:24 +0300

    5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const req = require('request');
+const fs = require('fs');
+
+req({ method: 'GET', uri: process.argv[2] }, 'utf-8', function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  fs.appendFile(process.argv[3], body, 'utf-8', error => error ? console.log(error) : '');
+});
