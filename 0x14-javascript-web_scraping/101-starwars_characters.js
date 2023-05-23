7c21df3650780a0f6ff6eea87fe9afae39ee52b1
exuszee <exuszee@gmail.com>
2023-05-23 21:30:49 +0300

    8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const req = require('request');
+
+req('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+
+  const results = JSON.parse(body).characters;
+  for (let i = 0; i < results.length; i++) {
+    req(results[i], (err, resp, bd) => {
+      if (err) {
+        console.log(err);
+      }
+      const chars = JSON.parse(bd).name;
+      console.log(chars);
+    });
+  }
+});
