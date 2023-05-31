cc6e50171fd5dbee71cf13ff61a391b0f8b065fd
exuszee <exuszee@gmail.com>
2023-05-31 17:49:20 +0300

    9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,9 +1,25 @@
 /* global $ */
 'use strict';
 
-$(document).ready(function () {
-  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-    const hello = data.hello;
-    $('#hello').text(hello);
-  });
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+  method: 'GET',
+  headers: {
+    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    DNT: '1',
+    Connection: 'keep-alive',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site'
+  }
+};
+
+$.ajax(settings).done(function (response) {
+  const hello = response.hello;
+  $('#hello').text(hello);
+  console.log(response);
 });
